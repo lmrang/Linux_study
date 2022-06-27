1f5b30516591831ecea9f9e772600b79dc03947e
lmrang <limlr@naver.com>
2022-06-27 10:56:49 +0900

    220627 Tcp/Ip

---
+++ Work/Socket/main.c
@@ -0,0 +1,10 @@
+#include "common.h"
+
+int main()
+{
+	char* str = input();
+	print(str);
+	print("\n");
+	print("and");
+	return 0;
+}
---
+++ Work/Socket/name.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("  이름   : 임이랑\n");
+	printf("생년월일 : 960319\n");
+	return 0;
+}
